dfc214c1bd8c9b2bd5693fa236201b6740cab86f
MartinMartimeo <martin@martimeo.de>
2011-06-07 11:55:04 +0200

    Initalisation

---
+++ pygat.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# martin@martimeo.de
+# Do not use without permission or care
+
+__author__  = "MartinMartimeo <martin@martimeo.de>"
+__date__    = "$07.06.2011 11:54:12$"
+
+if __name__ == "__main__":
+    print "Hello World"
